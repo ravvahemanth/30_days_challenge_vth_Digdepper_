4cce6cff16deaea21399a598512746b91a9213a3
ravva Hemanth <ravvahemanth@gmail.com>
2026-03-12 22:12:51 +0530

    Time: 0 ms (100%), Space: 61.1 MB (83.82%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,20 +4,23 @@ class Solution {
 
         k=k%n;
 
-        rotate(arr,0,n-1);
-        rotate(arr,0,k-1);
-        rotate(arr,k,n-1);
+       rotateby(arr,0,n-1);
+       rotateby(arr,0,k-1);
+       rotateby(arr,k,n-1);
+
+       
     }
 
-    public static void rotate(int arr[],int start , int end){
-      while(start < end){
-           int temp=arr[start];
-           arr[start]=arr[end];
-           arr[end]=temp;
 
-           start++;
-           end--;
+    public static void rotateby(int arr[],int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
 
-       }
+
+            start++;
+            end--;
+        }
     }
 }
